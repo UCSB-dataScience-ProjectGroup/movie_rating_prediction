53bd4a7f6f0788152c1daae0f00df071137172ce
Andrew Polk <polk@umail.ucsb.edu>
2018-01-30 03:14:10 -0800

    Cleaned up file

--- src/dataCall_example.py
+++ src/dataCall_example.py
@@ -3,7 +3,10 @@ from data.FactorQuery import FactorQuery as FQ
 from data.SaveLoadJson import SaveLoadJson as SLJ
 from data.GetParameters import GetParameters as GP
 
-filename = 'parameters.txt'
+# Replace with statistics script -----------------------------------------------
+from models.myStats import myStats as stats
+# ------------------------------------------------------------------------------
+
 movie = "0"
 
 while movie != "":
@@ -11,11 +14,8 @@ while movie != "":
     movie = input("Enter a movie name! \n")
     print(" ")
 
-    GP.find(movie) #Danielle and Brandon's code
+    GP.find(movie,debug=True) #Danielle and Brandon's code
 
-    FQ.getFactors() #get ratings for those parameters //my code
+    FQ.getFactors(debug=True) #get ratings for those parameters //my code
 
-    #analyze() the ratings and get rating #Ari, Ivy, Jake's code
-
-    data = SLJ.load(filename) #to load ratings into json object
-    print(json.dumps(data, indent=2))
+    stats.analyze() #the ratings and get rating #Ari, Ivy, Jake's code
