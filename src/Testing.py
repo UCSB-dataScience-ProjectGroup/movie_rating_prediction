979cb9d17f1e74dba18896387a09cbc8ce13ab39
Andrew Polk <polk@umail.ucsb.edu>
2018-01-30 03:16:57 -0800

    Can run scripts on N movies (FOR TESTING STATS ONLY)

---
+++ src/Testing.py
@@ -0,0 +1,47 @@
+import json
+import random
+from data.FactorQuery import FactorQuery as FQ
+from data.SaveLoadJson import SaveLoadJson as SLJ
+from data.GetParameters import GetParameters as GP
+from data.Search import Search
+
+# Replace with statistics python script ----------------------------------------
+from models.myStats import myStats as stats
+# ------------------------------------------------------------------------------
+
+#Enter number of movie to check
+print(" ")
+loops = input("Enter movies to check: \n")
+print(" ")
+
+#Initialize average values
+totalError = 0.0
+times = int(loops)
+
+#Loops chosen times
+for i in range(int(loops)):
+
+    #Find random ID value
+    ids = 2
+    valid = False
+    while valid == False:
+        ids = random.randint(2,489280)
+        #Make sure random value has non-0 rating
+        tempVal = Search.find(ids)
+        if tempVal != "NULL":
+            if float(tempVal.split('\t')[2]) > 0:
+                valid = True
+
+    #Get parameters and ratings
+    GP.get(ids,debug=True)
+    FQ.getFactors(debug=True)
+
+    #Get Average rating and average % error
+    Error = stats.analyze()
+    if Error != 100:
+        totalError += Error
+    else:
+        times -= 1
+
+#Print total error
+print("\nTotal Average error: ", format(totalError/int(loops),'.2f'),"%")
